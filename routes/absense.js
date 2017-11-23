575fa37051d4f48fd04f1990000fa084e54115e2
Oleg Apostol <truerenton@gmail.com>
2017-11-24 00:17:30 +0300

    v0.0.2

--- routes/absense.js
+++ routes/absense.js
@@ -3,148 +3,149 @@ import { ObjectID } from 'mongodb'
 import { getDatesList } from '../utils/date.js'
 
 const getQuery = params => {
-    const query = {}
+  const query = {}
 
-    if (!params) {
-        return {}
+  if (!params) {
+    return {}
+  }
+
+  const {
+    email,
+    status,
+    from,
+    to } = params
+
+  if (from && to) {
+    let days = getDatesList(new Date(from), new Date(to))
+
+    query.days = { $in: days }
+  }
+
+  if (email) {
+    query.email = {
+      $eq: email
     }
+  }
 
-    const {
-        email,
-        status,
-        from,
-        to } = params
-
-    if (from && to) {
-        let days = getDatesList(new Date(from), new Date(to))
-
-        query.days = { $in: days }
+  if (status) {
+    query.status = {
+      $eq: status
     }
+  }
 
-    if (email) {
-        query.email = {
-            $eq: email
-        }
-    }
-
-    if (status) {
-        query.status = {
-            $eq: status
-        }
-    }
-
-    return query
+  return query
 }
 
 const getUpdateData = ({ from, to, status }) => {
-    const data = {}
+  const data = {}
 
-    if (from && to) {
-        data.days = getDatesList(new Date(from), new Date(to))
-    }
+  if (from && to) {
+    data.days = getDatesList(new Date(from), new Date(to))
+  }
 
-    if (status) {
-        data.status = status
-    }
+  if (status) {
+    data.status = status
+  }
 
-    return data
+  return data
 }
 
 const getIntersections = (records, requestedDays) => {
-    return records.reduce((acc, { status, days }) => {
-        let intersectedDays = days.filter(day => (
-            requestedDays.some(reqDay => (
-                reqDay.getTime() === day.getTime()
-            ))
-        ))
+  return records.reduce((acc, { status, days }) => {
+    let intersectedDays = days.filter(day => (
+      requestedDays.some(reqDay => (
+        reqDay.getTime() === day.getTime()
+      ))
+    ))
 
-        if (intersectedDays.length > 0) {
-            acc.push({
-                status,
-                days,
-                intersectedDays
-            })
-        }
+    if (intersectedDays.length > 0) {
+      acc.push({
+        status,
+        days,
+        intersectedDays
+      })
+    }
 
-        return acc
-    }, [])
+    return acc
+  }, [])
 }
 
 export default (app, db) => {
-    const absense = new Collection(db, 'absense');
+  const absense = new Collection(db, 'absense');
 
-    app.get('/absense', ({ query }, res) => {
-        const params = getQuery(query)
+  app.get('/absense', ({ query }, res) => {
+    const params = getQuery(query)
 
-        absense.find(params)
-            .toArray((error, findResult) => {
-                if (error) {
-                    res.send({ error })
-                    return
-                }
+    absense.find(params)
+      .toArray((error, findResult) => {
+        if (error) {
+          res.send({ error })
+          return
+        }
 
-                res.send(findResult)
-            });
-    })
+        res.send(findResult)
+      });
+  })
 
-    app.post('/absense', ({ body }, res) => {
-        const {
-            email,
-            from,
-            to,
-            status
-        } = body
+  app.post('/absense', ({ body }, res) => {
+    const {
+      email,
+      from,
+      to,
+      status
+    } = body
 
-        const query = getQuery(body)
-        const requestedDays = getDatesList(new Date(from), new Date(to))
+    const query = getQuery(body)
+    const requestedDays = getDatesList(new Date(from), new Date(to))
 
-        // get records about absebce for specifyed user and time horizon
-        absense.find(query)
-            .toArray((error, records) => {
-                if (error) {
-                    res.send({ error })
+    // get records about absebce for specifyed user and time horizon
+    absense.find(query)
+      .toArray((error, records) => {
+        if (error) {
+          res.send({ error });
 
-                    return
-                }
+          return
+        }
 
-                if (records.length > 0) {
-                    const ovelapPeriods = getIntersections(records, requestedDays)
+        if (records.length > 0) {
+          const ovelapPeriods = getIntersections(records, requestedDays)
 
-                    if (ovelapPeriods.length > 0) {
-                        res.send({
-                            ovelapPeriods,
-                            error: 'Has overlaps. Please correct date range and try again.'
-                        })
-
-                        return
-                    }
-                }
-
-                const data = {
-                    email,
-                    days: requestedDays,
-                    status
-                }
-
-                absense.insertOne(data)
-                    .then(insertResult => res.send({
-                        status: 'OK',
-                        data: insertResult
-                    }))
-            });
-    })
-
-    app.put('/absense/:id', ({ params, body }, res) => {
-        const filter = { _id: ObjectID(params.id) }
-        const data = getUpdateData(body);
-
-        console.log('---- data', data)
-        absense.updateOne(filter, { $set: data })
-            .then(updateResult => {
-                res.send({
-                    status: 'OK',
-                    data: updateResult
-                })
+          if (ovelapPeriods.length > 0) {
+            res.send({
+              ovelapPeriods,
+              error: 'Has overlaps. Please correct date range and try again.'
             })
-    })
+
+            return
+          }
+        }
+
+        const data = {
+          email,
+          days: requestedDays,
+          status
+        }
+
+        absense.insertOne(data)
+          .then(insertResult => res.send({
+            status: 'OK',
+            data: insertResult
+          }));
+      });
+  });
+
+  app.put('/absense/:id', ({ params, body }, res) => {
+    const filter = { _id: ObjectID(params.id) }
+    const data = getUpdateData(body);
+
+    absense.updateOne(filter, { $set: data })
+      .then(updateRes => res.send(updateRes));
+  });
+
+  app.delete('/absense/:id', ({ params }, res) => {
+    const filter = { _id: ObjectID(params.id) }
+
+    absense.deleteOne(filter)
+      .then(deleteRes => res.send(deleteRes));
+  })
 }
--- routes/employee.js
+++ routes/employee.js
@@ -1,15 +1,12 @@
 import { Collection } from 'mongodbext'
+import { ObjectID } from 'mongodb'
 
 export default (app, db) => {
   const employees = new Collection(db, 'employee');
 
   app.get('/employee/:email?', ({ params }, res) => {
     const { email } = params
-    const query = {}
-
-    if (email) {
-      query.email = email
-    }
+    const query = email ? { email } : {}
 
     employees
       .find(query)
@@ -22,34 +19,37 @@ export default (app, db) => {
 
         res.send(findResult);
       });
-  })
+  });
 
   app.post('/employee', ({ body }, res) => {
-    const {
-      name,
-      email,
-      tel,
-      notes
-    } = body
+    const filter = { email: body.email }
+    const options = {
+      upsert: true,
+      new: true,
+      setDefaultsOnInsert: true
+    }
 
     employees
-      .findOne({ email })
-      .then(findResult => {
-        if (findResult) {
-          res.send({
-            error: 'User already exist.',
-            data: findResult
-          });
-
-          return
-        }
-
-        employees
-          .insertOne({ name, email, tel, notes })
-          .then(insertResult => res.send({
-            status: 'OK',
-            data: insertResult
-          }))
+      .findOneAndUpsert(filter, body, options)
+      .then((err, updateRes) => {
+        console.log(err, updateRes)
+        res.send(updateRes)
       });
+  });
+
+  app.put('/employee/:id', ({ params, body }, res) => {
+    const filter = { _id: ObjectID(params.id) }
+
+    employees
+      .updateOne(filter, { $set: body })
+      .then(updateRes => res.send(updateRes));
+  });
+
+  app.delete('/employee/:id', ({ params, body }, res) => {
+    const filter = { _id: ObjectID(params.id) }
+
+    employees
+      .deleteOne(filter, { $set: body })
+      .then(deleteRes => res.send(deleteRes));
   })
 }
